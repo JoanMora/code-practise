a283bfc6f587cc3d3ad5007c95f602ade381a93d
Joan Mora <elcorreudejoanmora@gmail.com>
2020-07-02 00:07:45 +0200

    Refactor

--- Python/Basics/Reverse_the_bits_in_an_integer/reverse_bits.py
+++ Python/Basics/Reverse_the_bits_in_an_integer/reverse_bits.py
@@ -7,7 +7,6 @@ You can assume that the number is not negative.
 '''
 
 def reverse_bits(n):
-    def reverse_bits(n):
     # use the in-build python function to get the binary
     # bin(number) returns 0b or -0b to indicate the sign followed by the actual bin number
     binary_number = bin(n)[2:] #.zfill(8) here there is no need of filling
